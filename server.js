55c6b8c16dda6974365997c1cb1d8566895b9c00
laurablanford <44933336+laurablanford@users.noreply.github.com>
2018-11-18 18:32:39 -0800

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.post('/update', function(req, res) {
             [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email, MailingPostalCode__c) VALUES ($1, $2, $3, $4, $5, $6)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim(), req.body.MailingPostalCode__c.trim()],
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email, MailingPostalCode) VALUES ($1, $2, $3, $4, $5, $6)',
+                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim(), req.body.MailingPostalCode.trim()],
                   function(err, result) {
                     done();
                     if (err) {
